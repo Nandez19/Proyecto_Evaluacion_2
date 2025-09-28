b2a1423864075a343a8841e608a911fcae03dd4f
Santi160206 <santialvarez1602gmail.com>
2025-09-28 01:26:22 -0500

    Borre todo

--- src/entities/__init__.py
+++
@@ -1,9 +0,0 @@
-from .autor import Autor
-from .bibliotecario import Bibliotecario
-from .editorial import Editorial
-from .libro import Libro
-from .prestamo import Prestamo
-from .usuario import Usuario
-
-
-__all__ = ["Autor", "Bibliotecario", "Editorial", "Libro", "Prestamo", "Usuario","Bibliotecario"]
