b6fcd76f77f462b18a91032544194381b9f55b5e
Nandez19 <juanpaho2005@gmail.com>
2025-10-09 00:34:49 -0500

    Organización de carpeta auth

--- src/controller/auth_controller.py
+++ src/controller/auth_controller.py
@@ -5,8 +5,8 @@ from uuid import UUID, uuid4
 from sqlalchemy.orm import Session
 
 from src.auth.jwt_handler import create_access_token, get_password_hash, verify_password
-from src.entities.Usuario import Usuario
-from src.shemas.auth import LoginRequest, UserCreate, UserResponse
+from src.entities.usuario import Usuario
+from src.schemas.auth import LoginRequest, UserCreate, UserResponse
 
 
 # ---------------------------
@@ -17,7 +17,7 @@ def create_user(db: Session, user: UserCreate) -> Usuario:
     Crea un nuevo usuario en la base de datos.
     """
     # Verificar si el nombre ya existe
-    existing_user = get_user_by_nombre(db, user.Nombre)
+    existing_user = get_user_by_username(db, user.Username)
     if existing_user:
         raise ValueError("El nombre de usuario ya existe")
 
@@ -27,14 +27,16 @@ def create_user(db: Session, user: UserCreate) -> Usuario:
         raise ValueError("El correo electrónico ya está registrado")
 
     # Crear nuevo usuario
-    hashed_password = get_password_hash(user.password)
+    Hashed_Password = get_password_hash(user.Password)
     db_user = Usuario(
-        Cedula_Usuario=uuid4(),
-        Nombre=user.Nombre,
-        Telefono=user.Telefono,
-        Correo=user.Correo,
-        password_hash=hashed_password,
-        Rol=user.Rol,
+        Id_Usuario = uuid4(),
+        Username = user.Username,
+        Correo = user.Correo,
+        Nombre = user.Nombre,
+        Password_Hash = Hashed_Password,
+        Rol = user.Rol,
+        Activo = True,
+        Fecha_creacion=datetime.utcnow(),
     )
 
     db.add(db_user)
@@ -46,7 +48,7 @@ def create_user(db: Session, user: UserCreate) -> Usuario:
 # ---------------------------
 # FUNCIONES AUXILIARES
 # ---------------------------
-def get_user_by_nombre(db: Session, nombre: str) -> Optional[Usuario]:
+def get_user_by_username(db: Session, nombre: str) -> Optional[Usuario]:
     return db.query(Usuario).filter(Usuario.Nombre == nombre).first()
 
 
@@ -55,5 +57,25 @@ def get_user_by_email(db: Session, correo: str) -> Optional[Usuario]:
 
 
 def get_user_by_id(db: Session, user_id: UUID) -> Optional[Usuario]:
-    return db.query(Usuario).filter(Usuario.Cedula_Usuario == user_id).first()
+    return db.query(Usuario).filter(Usuario.Id_Usuario == user_id).first()
+
+def aunthenticate_user(db: Session, username: str, password: str) -> Optional[Usuario]:
+    user = get_user_by_username(db, username)
+    if not user:
+        return None
+    if not verify_password(password, user.Password_Hash):
+        return None
+    if not user.Activo:
+        return None
+    return user
+
+def create_user_token(user:Usuario) -> str:
+    token_data = {
+        "sub": user.Username,
+        "user_id": str(user.Id_Usuario),
+        "rol": user.Rol
+    }
+    return create_access_token(data = token_data)
+
+
 
