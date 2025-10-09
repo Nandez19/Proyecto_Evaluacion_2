f5cc8e8dd729498839257ca76c560075ef5410f8
Nandez19 <juanpaho2005@gmail.com>
2025-10-09 00:36:20 -0500

    Actualización del init de entities

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -3,7 +3,7 @@ from .bibliotecario import Bibliotecario
 from .editorial import Editorial
 from .libro import Libro
 from .prestamo import Prestamo
-from .usuario import Usuario
+from .cliente import Cliente
 
 
-__all__ = ["Autor", "Bibliotecario", "Editorial", "Libro", "Prestamo", "Usuario"]
+__all__ = ["Autor", "Bibliotecario", "Editorial", "Libro", "Prestamo", "Cliente"]
