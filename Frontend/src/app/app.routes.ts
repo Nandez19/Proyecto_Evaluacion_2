5664780cef6c91cabe34ce7d6bc4c86fa6d23c4a
Santi160206 <santialvarez1602gmail.com>
2025-10-25 10:56:23 -0500

    Implementar otro Front

--- Frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { InicioComponent } from './Pages/inicio/inicio';
-
-export const routes: Routes = [
-  { path: '', component: InicioComponent }
-];
