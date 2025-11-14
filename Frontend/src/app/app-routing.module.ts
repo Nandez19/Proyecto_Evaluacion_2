486425643873671f08b4fcf09296e63327ba74f1
Nandez19 <juanpaho2005@gmail.com>
2025-11-14 16:38:29 -0500

    Cambios en el navbar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
-  },
+
+  { path: '', redirectTo: 'auth', pathMatch: 'full' }, 
+  
   {
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
+  
+
+  {
+    path: '', 
+    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+  },
+  
   {
     path: 'errors',
     loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
   },
+  
   { path: '**', redirectTo: 'errors/404' },
 ];
 
