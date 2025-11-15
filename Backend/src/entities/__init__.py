b55e1553522040fa8e6164a8af5e57e8fbf679c5
Nandez19 <juanpaho2005@gmail.com>
2025-11-14 22:10:26 -0500

    Eliminar Backend

--- Backend/src/entities/__init__.py
+++
@@ -1,9 +0,0 @@
-from .autor import Autor
-from .bibliotecario import Bibliotecario
-from .editorial import Editorial
-from .libro import Libro
-from .prestamo import Prestamo
-from .cliente import Cliente
-
-
-__all__ = ["Autor", "Bibliotecario", "Editorial", "Libro", "Prestamo", "Cliente"]
--- Backend/src/schemas/password_reset.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class ForgotPasswordRequest(BaseModel):
-    correo: EmailStr
-
-class ResetPasswordRequest(BaseModel):
-    token: str
-    nueva_contraseña: str
